d1441adb503b30140b9e8c9899086e72cb56adf8
rgx-design <rgx-design@users.noreply.github.com>
2026-07-05 19:54:12 +0800

    Remove light cyan background from all insect images

--- remove_background.py
+++ remove_background.py
@@ -3,6 +3,14 @@ import os
 
 PNG_DIR = 'png'
 
+def isLightCyan(r, g, b):
+    brightness = (r + g + b) / 3
+    if brightness < 180:
+        return False
+    green_blue_diff = abs(g - b)
+    r_ratio = r / max(brightness, 1)
+    return green_blue_diff < 40 and r_ratio < 0.95 and g > 180 and b > 180
+
 for filename in os.listdir(PNG_DIR):
     if not filename.endswith('.png'):
         continue
@@ -15,16 +23,18 @@ for filename in os.listdir(PNG_DIR):
         datas = img.getdata()
         
         new_data = []
+        removed = 0
         for item in datas:
             r, g, b, a = item
-            if r > 240 and g > 240 and b > 240:
+            if (r > 240 and g > 240 and b > 240) or isLightCyan(r, g, b):
                 new_data.append((r, g, b, 0))
+                removed += 1
             else:
                 new_data.append(item)
         
         img.putdata(new_data)
         img.save(filepath)
-        print(f'  Done: {filename}')
+        print(f'  Done: {filename} (removed {removed} pixels)')
         
     except Exception as e:
         print(f'  Failed: {filename} - {e}')
